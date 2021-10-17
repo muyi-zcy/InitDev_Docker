206ab8f4c75bfb3a6949e665ff47d785130c0846
muyi <zcy_nemo@aliyun.com>
2021-10-17 11:16:48 +0800

    安装docker-compose脚本

--- dockerConfig.sh
+++ dockerConfig.sh
@@ -19,3 +19,7 @@ sudo usermod -aG docker $USER
 
 echo '测试docker安装结果：'
 docker run --rm hello-world
+
+echo '安装docker-compose'
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod 755  /usr/local/bin/docker-compose
